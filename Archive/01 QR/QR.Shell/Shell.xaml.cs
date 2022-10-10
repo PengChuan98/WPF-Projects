d2e22927e45e8e08e6a2331f535150c31fedbc00
chuan <1114003209@qq.com>
2022-10-10 11:18:16 +0800

    初始界面

--- Archive/01 QR/QR.Shell/Shell.xaml.cs
+++ Archive/01 QR/QR.Shell/Shell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QR.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace QR.Shell
         public Shell()
         {
             InitializeComponent();
+            //ShellViewModel vm = new();
+            //this.window.DataContext = vm;
         }
     }
 }
